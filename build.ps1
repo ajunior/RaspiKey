da24ab4954ffdc0bf44efbb8c9729f8ef47b8f1d
samartzidis <samartzidis@gmail.com>
2018-12-31 19:27:29 +0000

    updated

--- build.ps1
+++ build.ps1
@@ -4,16 +4,30 @@ $msbuild_path="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MS
 $platform="ARM"
 $configuration="Release"
 
-Set-Location raspikey
-& $msbuild_path raspikey.sln /property:Configuration=$configuration /property:Platform=$platform
-Set-Location ..
+try 
+{
+    Set-Location raspikey
+    & $msbuild_path raspikey.sln /property:Configuration=$configuration /property:Platform=$platform
+    if($LastExitCode -ne 0) {
+        throw "raspikey.sln build failed"
+    }
 
-Set-Location raspikey-ui
-yarn install
-yarn build
-Set-Location ..
+    Set-Location raspikey-ui
+    & yarn install
+    if($LastExitCode -ne 0) {
+        throw "yarn install failed"
+    }
+    & yarn build
+    if($LastExitCode -ne 0) {
+        throw "yarn build failed"
+    }
 
-Copy-Item -Force -Path .\raspikey\bin\$platform\$configuration\raspikey .\setup\setup
-Copy-Item -Recurse -Force -Path .\raspikey-ui\build .\setup\setup\html
+    Copy-Item -Force -Path .\raspikey\bin\$platform\$configuration\raspikey .\setup\setup
+    Copy-Item -Recurse -Force -Path .\raspikey-ui\build .\setup\setup\html
 
-Compress-Archive -CompressionLevel Optimal -Path .\setup\* raspikey.zip
+    Compress-Archive -CompressionLevel Optimal -Path .\setup\* raspikey.zip
+}
+finally
+{
+    Set-Location $PSScriptRoot
+}
